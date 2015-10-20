c85bf38b8593d50c2ef1cdab65c98270ba6bc01b
Cristian Boboc <cristi@pidginhost.com>
2015-10-20 19:34:16 +0300

    updated project for pip

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-# paysafecard-python
+# paysafecard
 A class that implements the classic payment function of paysafecard
 
 Soap client rinse
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+setup(
+    name='paysafecard',
+    version='0.1',
+    packages=['paysafecard'],
+    url='https://github.com/opencbsoft/paysafecard-python',
+    license='MIT',
+    author='Cristian Boboc',
+    author_email='cristi@pidginhost.com',
+    description='A paysafecard class to help with classic payment api.',
+    long_description=long_description,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='paysafecard payment class',
+    install_requires=['suds-jurko'],
+)
